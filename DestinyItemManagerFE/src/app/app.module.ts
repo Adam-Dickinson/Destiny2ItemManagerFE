e4a04e473484ba05910872044b47044cb8904ca5
Adam Dickinson <adamgamesyt@gmail.com>
2023-09-09 11:32:12 +0200

    Added Angualr Materials, added dashboard page,

--- DestinyItemManagerFE/src/app/app.module.ts
+++ DestinyItemManagerFE/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    AuthCallbackComponent
+    AuthCallbackComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DestinyItemManagerFE/src/app/auth-callback/auth-callback.component.ts
+++ DestinyItemManagerFE/src/app/auth-callback/auth-callback.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';  // <-- Make sure to import Router
 import { AuthService } from '../services/AuthService';
 
 @Component({
@@ -7,12 +7,15 @@ import { AuthService } from '../services/AuthService';
   templateUrl: './auth-callback.component.html',
   styleUrls: ['./auth-callback.component.css']
 })
-
 export class AuthCallbackComponent implements OnInit {
 
   success: boolean = false;
 
-  constructor(private route: ActivatedRoute, private authService: AuthService) {}
+  constructor(
+    private route: ActivatedRoute, 
+    private authService: AuthService,
+    private router: Router  // <-- Inject the Router service
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
@@ -20,6 +23,7 @@ export class AuthCallbackComponent implements OnInit {
       if (userToken) {
         this.authService.userToken = userToken;
         this.success = true;
+        this.router.navigate(['/dashboard']);  // <-- Navigate to dashboard or any other route
       } else {
         this.success = false;
       }
